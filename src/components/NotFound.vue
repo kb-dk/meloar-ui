6bb228078fe96f899c3e81435b4bb8400ae2ae69
Jesper Lauridsen <jla@kb.dk>
2020-05-13 16:24:40 +0200

    Some minor cleanup

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="notfoundContainer">
-    <div v-if="ready === true" class="notfound">
+    <div class="notfound">
       <span class="unfortunate">Uh-oh,</span>
       <div class="error">404</div>
       <div class="message">
         <span>It looks like the page you looked for has been misplaced, and can't be reached. We're truly sorry!</span>
         <br>
         <router-link v-if="this.$route.name === 'Record'" :to="{name: 'Instance', params: { instance: this.$route.params.instance }}">← Go back to search</router-link>
-        <router-link v-if="this.$route.name === 'Instance'" :to="{name: 'Home' }">← Go back to Meloar</router-link>
+        <router-link v-if="this.$route.name !== 'Record'" :to="{name: 'Home' }">← Go back to Meloar</router-link>
       </div>
       </div>
   </div>
@@ -16,14 +16,8 @@
   <script>  
     export default {
       name: "NotFound",
-      data() {
-        return {
-          ready: false
-        };
-      },
       created() {
         console.log(this.$route)
-        this.ready = true
       },
       methods: {
       }
