1dfea71dae1896880ef6e465cdcee2f823edf1b2
Jesper Lauridsen <jla@kb.dk>
2020-06-16 15:37:56 +0200

    minor css adjustment changed text from danish to english (which we defaulted to)

--- src/containers/MeloarContainer.vue
+++ src/containers/MeloarContainer.vue
@@ -3,7 +3,7 @@
       <div class="titleContainer">
         <h2>Meloar</h2>
         <hr style="width:300px" />
-        <span>Tilgængelige MeLOAR instanser:</span>
+        <span>Available MeLOAR instances:</span>
       </div>
       <div class="simpleNavigation">
         <router-link v-bind:key="index" v-for="(item, index) in instances" class="menuLink instanceLink" :to="{name: 'Instance', params: { instance: item.key }}">{{ item.name }}</router-link>
